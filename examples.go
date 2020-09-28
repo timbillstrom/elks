96ef5e15ab0072feff2e7520da641e273acf3036
Tim Billström <tim@twinmedia.dev>
2020-09-28 17:55:38 +0100

    Updated examples.go

--- examples.go
+++ examples.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"log"
-	"os"
 )
 
-var localClient *Elks46
-
-func init() {
-	localClient = NewClient(os.Getenv("46_USERNAME"), os.Getenv("46_SECRET"), true)
-}
-
 func sendTestExample() {
-	log.Println(localClient.SendMessage(&SMS{
+	log.Println(NewClient("<USERNAME>", "<SECRET>", false).SendMessage(&SMS{
 		From:    "Github",
 		To:      "+46…",
 		Message: "Hej! 👋",
